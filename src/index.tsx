7d3c2060f932073ebeddeb2a131767c94ef454f1
kptankhoa <kptankhoa@gmail.com>
2023-11-03 10:18:35 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,5 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,11 @@
-import { Button, FullPageContainer } from 'components';
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { PAGE_ROUTE } from 'constant';
 
-const Home = () => {
-  const navigate = useNavigate();
-
-  return (
-    <FullPageContainer
-      content="Job Management"
-      subContent="by kptankhoa"
-    >
-      <Button
-        variant="outlined"
-        color="primary"
-        onClick={() => navigate(PAGE_ROUTE.JOBS)}
-        style={{ marginTop: 10, width: 200 }}
-      >
-        Job List
-      </Button>
-    </FullPageContainer>
-  );
-};
+const Home = () => (
+  <Navigate
+    to={PAGE_ROUTE.JOBS}
+  />
+);
 
 export default Home;
